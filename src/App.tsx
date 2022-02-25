04b3b670dbab55fa1dc57afbc100fe409ac5d481
Troy DeSpain <despaintroy@gmail.com>
2022-02-24 20:54:12 -0700

    Setup styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Typography } from '@mui/material'
+import React from 'react'
+import { FC } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: FC = () => {
+	return (
+		<div className='App'>
+			<Container style={{ marginTop: '1rem' }}>
+				<Typography variant='h1'>Welcome to React</Typography>
+				<Typography>Welcome to the react app!</Typography>
+				<Button
+					variant='contained'
+					color='secondary'
+					onClick={(): void => console.log('Hello!')}
+				>
+					Say Hello!
+				</Button>
+			</Container>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/theme.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+import {
+	Link as RouterLink,
+	LinkProps as RouterLinkProps,
+} from 'react-router-dom'
+
+import { colors } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
+
+// Allow adapting MUI links to react-router-dom links
+const LinkBehavior = React.forwardRef<
+	any,
+	Omit<RouterLinkProps, 'to'> & { href: RouterLinkProps['to'] }
+>((props, ref) => {
+	const { href, ...other } = props
+	return <RouterLink ref={ref} to={href} {...other} />
+})
+
+LinkBehavior.displayName = 'LinkBehavior'
+
+const theme = createTheme({
+	type: 'dark',
+	palette: {
+		primary: {
+			main: colors.blueGrey[900],
+		},
+		secondary: {
+			main: colors.lightBlue[400],
+			dark: colors.lightBlue[500],
+		},
+	},
+	components: {
+		MuiLink: {
+			defaultProps: {
+				// @ts-ignore
+				component: LinkBehavior,
+			},
+		},
+		MuiButtonBase: {
+			defaultProps: {
+				LinkComponent: LinkBehavior,
+			},
+		},
+		// MuiAppBar: {
+		// 	styleOverrides: {
+		// 		colorPrimary: {
+		// 			backgroundColor: '#fff',
+		// 			color: colors.blueGrey[800],
+		// 		},
+		// 	},
+		// },
+		MuiListItemButton: {
+			styleOverrides: {
+				root: {
+					borderRadius: 5,
+					margin: '0.5rem',
+				},
+			},
+		},
+	},
+	typography: {
+		allVariants: {
+			color: colors.blueGrey[200],
+		},
+		h1: {
+			color: colors.blueGrey[100],
+			fontSize: '1.8rem',
+			fontWeight: '500',
+		},
+		h2: {
+			color: colors.blueGrey[100],
+			fontSize: '1.3rem',
+			fontWeight: '500',
+		},
+		h3: {
+			color: colors.blueGrey[100],
+			fontSize: '1.1rem',
+			fontWeight: '500',
+		},
+	},
+})
+
+export default theme
