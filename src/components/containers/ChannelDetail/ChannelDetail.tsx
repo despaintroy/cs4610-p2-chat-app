0445c5b6d2a321effb59eac5f38f730b1f076846
Troy DeSpain <despaintroy@gmail.com>
2022-02-27 22:08:08 -0700

    Add send message field

--- src/components/containers/ChannelDetail/ChannelDetail.tsx
+++ src/components/containers/ChannelDetail/ChannelDetail.tsx
@@ -1,8 +1,15 @@
-import { Avatar, Stack, Typography } from '@mui/material'
+import {
+	Avatar,
+	InputAdornment,
+	OutlinedInput,
+	Stack,
+	Typography,
+} from '@mui/material'
 import { Box } from '@mui/system'
 import React, { FC, useContext, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 import { ServersContext } from 'AuthHome'
+import SendIcon from '@mui/icons-material/Send'
 
 const ChannelDetail: FC = () => {
 	const servers = useContext(ServersContext) || []
@@ -25,47 +32,62 @@ const ChannelDetail: FC = () => {
 		? [...channel.messages, ...channel.messages, ...channel.messages]
 		: []
 
-	console.log(currentServer)
-
 	return (
-		<Box className='messages-container' sx={{ backgroundColor: '#37393e' }}>
-			{lotsMessages?.map((message, index) => {
-				const userProfile = currentServer?.userProfiles?.find(
-					profile => profile.userId === message.userId
-				)
+		<Stack direction='column'>
+			<Box className='messages-container' sx={{ backgroundColor: '#37393e' }}>
+				{lotsMessages?.map((message, index) => {
+					const userProfile = currentServer?.userProfiles?.find(
+						profile => profile.userId === message.userId
+					)
 
-				return (
-					<Box key={index} className='message'>
-						<Stack direction='row'>
-							<Avatar
-								sx={{ mr: 2, mt: 1 }}
-								src={userProfile?.profileImage || undefined}
-							/>
-							<Box sx={{ width: '100%' }}>
-								<Stack direction='row'>
-									<Box className='user-name'>
-										{userProfile?.name || <i>Removed</i>}
+					return (
+						<Box key={index} className='message'>
+							<Stack direction='row'>
+								<Avatar
+									sx={{ mr: 2, mt: 1 }}
+									src={userProfile?.profileImage || undefined}
+								/>
+								<Box sx={{ width: '100%' }}>
+									<Stack direction='row'>
+										<Box className='user-name'>
+											{userProfile?.name || <i>Removed</i>}
+										</Box>
+										<Box className='time' sx={{ ml: 'auto' }}>
+											<Typography variant='caption' color='text.disabled'>
+												{(message.timestamp &&
+													new Date(message.timestamp)
+														.toISOString()
+														.split('T')[0]) ||
+													'-'}
+											</Typography>
+										</Box>
+									</Stack>
+									<Box className='content' color='#d9dadb'>
+										{message.content}
 									</Box>
-									<Box className='time' sx={{ ml: 'auto' }}>
-										<Typography variant='caption' color='text.disabled'>
-											{(message.timestamp &&
-												new Date(message.timestamp)
-													.toISOString()
-													.split('T')[0]) ||
-												'-'}
-										</Typography>
-									</Box>
-								</Stack>
-								<Box className='content' color='#d9dadb'>
-									{message.content}
 								</Box>
-							</Box>
-						</Stack>
-					</Box>
-				)
-			})}
-			<div ref={messagesEndRef} />
-		</Box>
+							</Stack>
+						</Box>
+					)
+				})}
+				<div ref={messagesEndRef} />
+			</Box>
+			<Box sx={{ px: 1, pb: 3, bgcolor: '#37393e' }}>
+				<OutlinedInput
+					fullWidth
+					autoFocus
+					placeholder={`Message #${channel?.name}`}
+					size='small'
+					autoComplete='off'
+					endAdornment={
+						<InputAdornment position='end'>
+							<SendIcon />
+						</InputAdornment>
+					}
+					sx={{ bgcolor: '#41444A' }}
+				/>
+			</Box>
+		</Stack>
 	)
 }
 
